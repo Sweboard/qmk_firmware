6c06534bf71363fbc3c65469a1d220919561fe13
William Wennerström <william@willeponken.me>
2018-10-15 17:23:22 +0200

    mitosis/keymaps/swolemak: update layout and fix "<"-key issue
        * update layout
         - moved symbols (most used on home row)
         - left & right ctrl keys added
         - moved super key to right keyboard (to make room for the ctrl keys)
         - removed shift + tab key (can still be used by pressing shift + tab
         manually)
         - moved fn key to right side
         - switched brackets
        * fix issue w/ "<"-key, the shift state interfered with the NO_LESS
        keycode
        * use NO keys instead of KC

---
+++ keyboards/mitosis/keymaps/swolemak/rules.mk
@@ -0,0 +1,4 @@
+EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
+CONSOLE_ENABLE = yes          # Console for debug(+400)
+COMMAND_ENABLE = yes         # Commands for debug and configuration
+UNICODE_ENABLE = yes          # Unicode
